acd91da12a69101d99833d47506f5c2cd179af58
Drogolova <drogolova1997@mail.ru>
2022-10-18 00:09:28 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog - 4;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var Boxer1Weight = 78.2;
+        var Boxer2Weight = 82.7;
+        var TotalWeight = Boxer1Weight + Boxer2Weight;
+        System.out.println("Общий вес бойцов " + TotalWeight + " кг");
+        var DifferenceWay1 = Boxer2Weight - Boxer1Weight;
+        System.out.println("Разница между весами бойцов " + DifferenceWay1 + " кг");
+        var DifferenceWay2 = TotalWeight % Boxer1Weight;
+        System.out.println("Разница между весами бойцов " + DifferenceWay2 + " кг");
 
-        var friend = 19;
-                System.out.println(friend);
-                friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+        var TotalHours = 640;
+        var EmploeeHours = 8;
+        var AllEmploees = TotalHours / EmploeeHours;
+        System.out.println("Всего работников в компании - " + AllEmploees + " человек");
+        var NewAllEmploees = AllEmploees + 94;
+        var NewTotalHours  = NewAllEmploees * 8;
+        System.out.println("Если в компании работает " + NewAllEmploees + " человек, то всего " + NewTotalHours +
+                " часов работы может быть поделено между сотрудниками");
 
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+
+
+
     }
 
 }
